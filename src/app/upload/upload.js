c587736bac5cc65ca98605159ee9f01e405a4b84
Felixfranzen <felixfranzn@gmail.com>
2016-12-13 21:01:58 +0100

    pretty awesome file uploading

--- src/app/upload/upload.js
+++ src/app/upload/upload.js
@@ -25,17 +25,19 @@ function uploadController(apiFactory, authFactory, $state, $scope, currentAuth){
 	var db = apiFactory.sampleDataReference();
 	vm.sampleData = {};
 	vm.upload = upload;
+	vm.clear = clear;
 
 	function upload(){
 		if (!vm.audioFile){
 			return;
 		}
+
 		var category = vm.sampleData.category || 'loop';
 		var format = "." + vm.audioFile.type.replace('audio/', '');
-		var size = parseInt(vm.audioFile.size/1000, 10) + "kb";
+		var size = parseInt(vm.audioFile.size, 10);
 		var title = vm.sampleData.title || vm.audioFile.name;
 		var uploader = currentAuth.displayName || currentAuth.email;
-		
+
 		db.$add({
 			bitrate: "Unknown",
 			category: category,
@@ -48,14 +50,18 @@ function uploadController(apiFactory, authFactory, $state, $scope, currentAuth){
 			uploader: uploader,
 			url: ""
 		}).then(function(ref){
+			vm.uploadState = "uploading";
 			uploadTask = apiFactory.sampleStorageReference(ref.key).put(vm.audioFile);
 			uploadTask.on('state_changed', function(snapshot){
-				var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-				console.log(progress);
+				$scope.$evalAsync(function(){
+					var progress = parseInt((snapshot.bytesTransferred/snapshot.totalBytes)*100, 10) || 1;
+					vm.uploadProgress = progress+10;
+				});
 			}, function(error) {
 				//handle error
 
 			}, function() {
+				vm.uploadState = "finished";
 				record = db.$getRecord(ref.key);
 				record.url = uploadTask.snapshot.downloadURL;
 				db.$save(record);
@@ -64,9 +70,20 @@ function uploadController(apiFactory, authFactory, $state, $scope, currentAuth){
 		});
 	}
 
+	function clear(){
+		if (vm.uploadState === 'uploading'){
+			return;
+		}
+		vm.uploadState = "";
+		vm.audioFile = "";
+		vm.uploadProgress = 0;
+		vm.sampleData = {};
+	}
+
 	$scope.$watch('vm.audioFile', function(){
 		if (!vm.audioFile){
 			return;
 		}
+		vm.sampleData.title = vm.audioFile.name;
 	});
 }
