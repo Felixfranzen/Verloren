59dd0d0482a56d919457904175b29d9e03d42618
Felixfranzen <felixfranzn@gmail.com>
2016-12-25 00:38:51 +0100

    autofocus logn/signup fields + button active states

---
+++ src/app/common/utils.factory.js
@@ -0,0 +1,15 @@
+angular.module("verloren.utils", [])
+
+.factory("utils", utils);
+function utils(){
+
+	return {
+		parseFileSize: parseFileSize
+	};
+
+
+	function parseFileSize(size){
+
+	}
+
+}
